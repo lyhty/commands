b952b769b11880562eaf46a70626192330ea701e
Matti Suoraniemi <matti@suoraniemi.com>
2023-01-19 21:07:14 +0100

    Small change

--- src/EnumMakeCommand.php
+++ src/EnumMakeCommand.php
@@ -4,74 +4,72 @@ namespace Lyhty\Commands;
 
 use Symfony\Component\Console\Input\InputOption;
 
-if (version_compare(PHP_VERSION, '8.1', '>=')) {
-    class EnumMakeCommand extends GeneratorCommand
+class EnumMakeCommand extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $name = 'make:enum';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new PHP enum';
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Enum';
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param  string  $name
+     * @return string
+     */
+    protected function buildClass($name)
     {
-        /**
-         * The name and signature of the console command.
-         *
-         * @var string
-         */
-        protected $name = 'make:enum';
+        return $this->replaceBackedEnumType(
+            parent::buildClass($name)
+        );
+    }
 
-        /**
-         * The console command description.
-         *
-         * @var string
-         */
-        protected $description = 'Create a new PHP enum';
+    /**
+     * Replace the model for the given stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function replaceBackedEnumType($stub)
+    {
+        $typeOption = $this->option('backed');
 
-        /**
-         * The type of class being generated.
-         *
-         * @var string
-         */
-        protected $type = 'Enum';
+        $replace = [
+            '{{ enumType }}' => $typeOption ? ": $typeOption" : '',
+        ];
 
-        /**
-         * Build the class with the given name.
-         *
-         * @param  string  $name
-         * @return string
-         */
-        protected function buildClass($name)
-        {
-            return $this->replaceBackedEnumType(
-                parent::buildClass($name)
-            );
-        }
+        return str_replace(
+            array_keys($replace),
+            array_values($replace),
+            $stub
+        );
+    }
 
-        /**
-         * Replace the model for the given stub.
-         *
-         * @param  string  $stub
-         * @return string
-         */
-        protected function replaceBackedEnumType($stub)
-        {
-            $typeOption = $this->option('backed');
-
-            $replace = [
-                '{{ enumType }}' => $typeOption ? ": $typeOption" : '',
-            ];
-
-            return str_replace(
-                array_keys($replace),
-                array_values($replace),
-                $stub
-            );
-        }
-
-        /**
-         * Get the console command arguments.
-         *
-         * @return array
-         */
-        protected function getOptions()
-        {
-            return [
-                ['backed', 'b', InputOption::VALUE_OPTIONAL, 'Make a backed enum with given type.'],
-            ];
-        }
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['backed', 'b', InputOption::VALUE_OPTIONAL, 'Make a backed enum with given type.'],
+        ];
     }
 }
